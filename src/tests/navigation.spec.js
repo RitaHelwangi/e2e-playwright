ab4cbaef2323cfe96bc498fe08d3127b9a61463b
Rita Helwangi <rita.helwangi@gmail.com>
2025-06-04 15:45:03 +0200

    Navigering test: växla mellan vyer och visa rätt vyer

--- src/tests/navigation.spec.js
+++ src/tests/navigation.spec.js
@@ -9,4 +9,17 @@ test.describe("Navigering mellan vyer", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://tap-ht24-testverktyg.github.io/exam-template/");
   });
+
+  test("Navigerar från Katalog till Lägg till bok och sedan till Mina böcker visar rätt vyer", async ({
+    page,
+  }) => {
+    // Click  behövs inte här då det börjar med katalog
+    await expect(page.getByText("Flora Tistel")).toBeVisible();
+
+    await page.getByRole("button", { name: "Lägg till bok" }).click();
+    await expect(page.getByTestId("add-input-title")).toBeVisible();
+
+    await page.getByRole("button", { name: "Mina böcker" }).click();
+    await expect(page.getByText(/Dina favoriter/i)).toBeVisible();
+  });
 });
