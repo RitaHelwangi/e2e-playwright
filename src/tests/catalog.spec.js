f1cddf972b9a4d1471f2adb59f7bf16046cc1690
Rita Helwangi <rita.helwangi@gmail.com>
2025-06-04 17:32:54 +0200

    test för att verifiera synlighet av böcker i katalog-vyn

---
+++ src/tests/catalog.spec.js
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Katalog-vy", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://tap-ht24-testverktyg.github.io/exam-template/");
+  });
+
+  test("Visar en lista med tillgängliga böcker", async ({ page }) => {
+    await expect(
+      page.getByText("Hur man tappar bort sin TV-fjärr 10 gånger om dagen")
+    ).toBeVisible();
+    await expect(
+      page.getByText("Kaffekokaren som visste för mycket")
+    ).toBeVisible();
+    await expect(page.getByText("Min katt är min chef")).toBeVisible();
+    await expect(page.getByText("100 sätt att undvika måndagar")).toBeVisible();
+    await expect(
+      page.getByText("Gräv där du står – och hitta en pizzameny")
+    ).toBeVisible();
+    await expect(page.getByText("Jag trodde det var tisdag")).toBeVisible();
+    await expect(
+      page.getByText(
+        "Att prata med växter – och vad de egentligen tycker om dig"
+      )
+    ).toBeVisible();
+  });
+});
