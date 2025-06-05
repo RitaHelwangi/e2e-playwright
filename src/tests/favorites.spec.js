d09cbb98706312f21eee933c97042b7f84b28bf9
Rita Helwangi <rita.helwangi@gmail.com>
2025-06-05 15:11:01 +0200

    test för att visa när favoritlistan är tom

--- src/tests/favorites.spec.js
+++ src/tests/favorites.spec.js
@@ -7,13 +7,22 @@ test.describe("Mina böcker-vy", () => {
 	});
 	
 	test("visar en lista över mina favoritmarkerade böcker", async ({ page }) => {
-    await page.goto("https://tap-ht24-testverktyg.github.io/exam-template/");
-    await page.getByTestId("star-Kaffekokaren som visste för mycket").click();
-    await page.getByRole("button", { name: "Mina böcker" }).click();
-
-    await expect(
-      page.getByText("Kaffekokaren som visste för mycket")
-    ).toBeVisible();
-  });
-
+		await page.goto("https://tap-ht24-testverktyg.github.io/exam-template/");
+		await page.getByTestId("star-Kaffekokaren som visste för mycket").click();
+		await page.getByRole("button", { name: "Mina böcker" }).click();
+		
+		await expect(
+			page.getByText("Kaffekokaren som visste för mycket")
+		).toBeVisible();
+	});
+	
+	test("visar att favoritlistan är tom när inga böcker är markerade", async ({
+		page,
+	}) => {
+		await expect(
+			page.getByText("Kaffekokaren som visste för mycket")
+		).not.toBeVisible();
+		
+		await expect(page.getByText("När du valt, kommer dina favoritböcker att visas här")).toBeVisible();
+	});
 });
